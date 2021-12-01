cff78b5c8281db256bd01c1e6c181f0ba13e7c8a
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-12-02 00:10:25 +0400

    add new features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,36 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import Router from "next/router";
 import Head from "next/head";
+import NProgress from "nprogress";
+
+import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { isServer } from "../shared/is-server";
+import { PageComponent } from "../types/_app";
+import { ToastContainer } from "react-toastify";
+
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const store = configureStore({
   reducer: {},
 });
 
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }: AppProps) {
+  NProgress.settings.showSpinner = false;
+
+  if (
+    isServer &&
+    !Component.getInitialProps &&
+    (Component as PageComponent<unknown>).ws
+  ) {
+    return null;
+  }
   return (
     <>
       <Head>
@@ -23,7 +45,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width,minimum-scale=1,initial-scale=1,user-scalable=no"
         />
       </Head>
+
       <Provider store={store}>
+        <ToastContainer />
         <Component {...pageProps} />
       </Provider>
     </>
